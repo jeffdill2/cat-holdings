b4da332666793258e7aaf3a694f0fa1f53897fce
Jeffrey Dill <jeffdill2@gmail.com>
2015-03-26 21:00:14 -0400

    Testing something

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,7 +1,7 @@
 class WelcomesController < ApplicationController
 
 	def index
-
+		@number = Category.last
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150327003738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.integer  "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
